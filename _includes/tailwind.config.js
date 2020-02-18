041706574b89fe0aa0b7e2491bc571e61f1574bd
Nathan Selikoff <nselikoff@gmail.com>
2020-02-18 14:01:54 -0500

    add basic styling for mobile

--- _includes/tailwind.config.js
+++ _includes/tailwind.config.js
@@ -1,6 +1,35 @@
 module.exports = {
   theme: {
-    extend: {},
+    fontFamily: {
+      'mono': ['Share Tech Mono', 'monospace'],
+      'sans': ['Lato', 'sans-serif'],
+    },
+    fontSize: {
+      'xs': '0.75rem', // 12 px
+      'sm': '0.875rem', // 14 px
+      'base': '1rem', // 16 px
+      'lg': '1.3125rem', // 21 px
+      'xl': '1.75rem', // 28 px
+      '2xl': '2.375rem', // 38 px
+      '3xl': '3.1875rem', // 51 px
+      '4xl': '4.1875rem', // 67 px
+    },
+    extend: {
+      colors: {
+        accent: '#C0C019',
+        indigo: {
+          '100': '#EBEBFF',
+          '200': '#C2C2FF',
+          '300': '#9C9CFF',
+          '400': '#8080F6',
+          '500': '#6464ED',
+          '600': '#5151E8',
+          '700': '#3939DE',
+          '800': '#323290',
+          '900': '#30306B',
+        }
+      }
+    },
   },
   variants: {},
   plugins: [],
